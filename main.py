85e136fba8da3d03747a863d9c599a60abd43a90
WillPalaia <wpalaia.2022@gmail.com>
2023-11-11 15:38:17 -0500

    Gets URIs

--- main.py
+++ main.py
@@ -3,11 +3,76 @@ import os
 import base64
 from requests import post
 import json
+from spotipy import Spotify
 
 """
 CLIENT_ID = "5d3a61ce96b245adaf521468e1b8ed55"
 CLIENT_SECRET = "af2ea7921189463abfe526725b9ea465"
 """
+
+def get_album_uri(spotify: Spotify, name: str) -> str:
+    """
+    :param spotify: Spotify object to make the search from
+    :param name: album name
+    :return: Spotify uri of the desired album
+    """
+
+    # Replace all spaces in name with '+'
+    original = name
+    name = name.replace(' ', '+')
+
+    results = spotify.search(q=name, limit=1, type='album')
+    if not results['albums']['items']:
+        raise NameError(f'No album named "{original}"')
+    album_uri = results['albums']['items'][0]['uri']
+    return album_uri
+
+def get_artist_uri(spotify: Spotify, name: str) -> str:
+    """
+    :param spotify: Spotify object to make the search from
+    :param name: album name
+    :return: Spotify uri of the desired artist
+    """
+
+    # Replace all spaces in name with '+'
+    original = name
+    name = name.replace(' ', '+')
+
+    results = spotify.search(q=name, limit=1, type='artist')
+    if not results['artists']['items']:
+        raise NameError(f'No artist named "{original}"')
+    artist_uri = results['artists']['items'][0]['uri']
+    print(results['artists']['items'][0]['name'])
+    return artist_uri
+
+def get_track_uri(spotify: Spotify, name: str) -> str:
+    """
+    :param spotify: Spotify object to make the search from
+    :param name: track name
+    :return: Spotify uri of the desired track
+    """
+
+    # Replace all spaces in name with '+'
+    original = name
+    name = name.replace(' ', '+')
+
+    results = spotify.search(q=name, limit=1, type='track')
+    if not results['tracks']['items']:
+        raise NameError(f'No track named "{original}"')
+    track_uri = results['tracks']['items'][0]['uri']
+    return track_uri
+
+def play_album(spotify=None, device_id=None, uri=None):
+    spotify.start_playback(device_id=device_id, context_uri=uri)
+
+
+def play_artist(spotify=None, device_id=None, uri=None):
+    spotify.start_playback(device_id=device_id, context_uri=uri)
+
+
+def play_track(spotify=None, device_id=None, uri=None):
+    spotify.start_playback(device_id=device_id, uris=[uri])
+    
 
 load_dotenv()
 
