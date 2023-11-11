2a7959a1781f32a0a4ba20cce1416215211c7b77
Tomer Carmeli <tocarmeli@gmail.com>
2023-11-11 00:16:31 -0500

    added token

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv
 import os
 import base64
+from requests import post
+import json
 
 load_dotenv()
 
@@ -17,3 +19,12 @@ def get_token():
         "Authorization": "Basic " + auth_base64,
         "Content-Type": "application/x-www-form-urlencoded"
     }
+
+    data = {"grant_type": "client_credentials"}
+    result = post(url, headers=headers, data=data)
+    json_result = json.loads(result.content)
+    token = json_result["access_token"]
+    return token
+
+token = get_token()
+print(token)
