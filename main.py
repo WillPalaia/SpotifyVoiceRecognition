286692232f0afbf3ea5db28504df350e666535ac
Tomer Carmeli <tocarmeli@gmail.com>
2023-11-10 23:52:32 -0500

    add token

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 from dotenv import load_dotenv
 import os
+import base64
 
 load_dotenv()
 
 client_id = os.getenv("CLIENT_ID")
 client_secret = os.getenv("CLIENT_SECRET")
 
-print(client_id, client_secret)
+def get_token():
+    auth_string = client_id + ":" + client_secret
+    auth_bytes = auth_string.encode("utf-8")
+    auth_base64 = str(base64.b64encode(auth_base64), "utf-8")
